5da813aad1ea02751cfffc3631f8d852bdfcfca6
weih201 <weih201@gmail.com>
2014-04-25 19:40:05 +1000

    Submit

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,21 @@
-# Matrix inversion is usually a costly computation and their may be some benefit to caching the inverse of a matrix rather than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
-# 
-# Write the following functions:
-#   1.makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-# 2.cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache
+## File name: cachematrix.R
+
+## Martix inversion is usually computation expensively. A possible improvement is to chache a matrix inversion instead of 
+## compute it repeatly. This program is to provide a pair of functions which cache the inverse of a matrix. 
+
+## The two functions are:
+## 1.makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## 2.cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the 
+##               inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve 
+##               the inverse from the cache
 
 
-## makeVector creates a special "vector", which is really a list containing a function to 
-## 1.set the value of the vector
-## 2.get the value of the vector
-## 3.set the value of the mean
-## 4.get the value of the mean
+## makeCacheMatrix() function creates a special list which contains the functions to 
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the matrix inversion
+## 4.get the value of the matrix inversion
 makeCacheMatrix <- function(x = matrix()) {
-
   m <- NULL
   set <- function(y) {
     x <<- y
@@ -27,14 +31,14 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
-  
 }
 
 
-## The following function calculates the mean of the special "vector" created with the above function. 
-## However, it first checks to see if the mean has already been calculated. 
-## If so, it gets the mean from the cache and skips the computation. 
-## Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function
+## cacheSolve function calculates the inversion of the matrix which was created with the above function. 
+## Firstly, it checks to see if the inversion is existed with getsolve function. 
+## If so, it gets the inversion from the cache and skips the computation. 
+## Otherwise, it calculates the inversion of the matrix and sets the value of the inversion in the cache 
+## via the setsolve function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
